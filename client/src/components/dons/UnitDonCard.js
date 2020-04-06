bad4d46a85a87af6dac484b5a09d1154e5d8833c
Gregoire Lamy <g.aderlamy@gmail.com>
2020-04-06 21:41:03 +0200

    amélioration card

--- client/src/components/dons/UnitDonCard.js
+++ client/src/components/dons/UnitDonCard.js
@@ -15,26 +15,27 @@ class UnitDonCard extends React.Component {
     return (
       <div className="cardInfo">
 
-          <div className="productName"> 
-          <div>Produit: </div>
-          <div>{this.props.productName}</div>
-          </div>
-
-          <div className="productType">
-            <div>Type: {this.props.productType}</div>
+        <div className="productType">
+            <div>Type</div>
             <div>
               <img src={foodTypes[this.props.productType]} alt="food type" />
             </div>
           </div>
 
+          <div className="productName"> 
+          <div>Produit </div>
+          <div className="grasDroite">{this.props.productName}</div>
+          </div>
+
           <div className="productWeight">
-            <div> Poids: </div>
-            <div>{this.props.quantity.value}{" "}
+            <div> Poids </div>
+            <div className="grasDroite">{this.props.quantity.value}{" "}
             {" " + this.props.quantity.qtyType}</div>
           </div>
 
           <div className="productexpDate">
-            <div>Expire le: </div><div>{expiration}</div>
+            <div>Péremption </div>
+            <div className="grasDroite">{expiration}</div>
           </div>
       </div>
       
